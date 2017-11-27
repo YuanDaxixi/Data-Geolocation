46788a7662eec06ef1384e13fab5362edbd74033
YuanDaxixi <274505579@qq.com>
2017-11-27 22:20:50 +0800

    ignore it

--- PDP/cloud.py
+++ PDP/cloud.py
@@ -1,1 +1,26 @@
+# -*- coding: utf-8 -*-
+# user module
+# created by YuanDa 2017-11
 
+import socket, struct, timeit, random, time
+
+def retrieve_block(index, blocksize):
+    """return the file(pointed by fp) block according to 'index'"""
+    a = time.clock()
+    fp = open('test', 'rb')
+    offset = index * blocksize
+    fp.seek(offset)
+    block = fp.read(blocksize)
+    b = time.clock()
+    print (b - a) * 1000, 'ms'
+    return block
+
+def test_time(func):
+    time = timeit.timeit(func, 'from __main__ import retrieve_block')
+    print time
+
+#test_time('retrieve_block(0, 4096)')
+a = time.clock()
+retrieve_block(0, 4096)
+b = time.clock()
+print (b - a) * 1000, 'ms'
--- PDP/user.py
+++ PDP/user.py
@@ -15,10 +15,12 @@ BUFF_SIZE = 1024
 
 ################# Key Generation #################
 
-# key generation
-# 'mode' = 0 means symmetric key, 1 for public key
-# 'key_len' is key length
+
 def gen_key(key_len, mode = 0):
+    """ key generation, return key or False
+    mode -- 0 for symmetric key, 1 for public key
+    key_len -- key length
+    """
     if mode == 0:
         key = random.getrandbits(key_len)
         return (key,)
@@ -28,8 +30,8 @@ def gen_key(key_len, mode = 0):
         print 'Wrong mode, 0 or 1'
         return False
 
-# retrieve key from gen_key()
 def get_tag_key(key, mode = 0):
+    """retrieve key from gen_key(), return key in bytes"""
     if mode == 0:
         tag_key = get_secret_key(key) # symmtric key implementation
     elif mode == 1:
@@ -60,8 +62,8 @@ def get_private_key(key):
         print 'illegal public key pair'
         return False
 
-# store the key in file named 'file_name'
 def store_key(key, file_name):
+    """ store the key in file named 'file_name' """
     with open(file_name, 'wb') as fp:
         for item in key:
             fp.write(random.long_to_bytes(item))
@@ -72,14 +74,15 @@ def store_key(key, file_name):
 # the number of blocks of the file expected handled
 block_count = 0
 
-# input file F, generate metadata, save it, and send new file to cloud
-# invoked for user->cloud client 
-# 'file_name' is input file F
-# 'output_name' is new file store tags
-# 'blocksize' is the size(bytes) of the block
-# 'mode' is 0 for symmtric key, 1 for public key
-# 'server_sock' is the socket
 def pdp_setup(*args):
+    """input file F, generate metadata, save it, and send new file to cloud.
+       invoked for user->cloud client.
+     file_name -- input file F
+     output_name -- new file store tags
+     blocksize -- size(bytes) of the block
+     mode -- 0 for symmtric key, 1 for public key
+     server_sock -- socket
+    """
     file_name, output_name, block_size, mode, server_sock = args # interface between user and client
     # generate tag and key, then store them
     try:
@@ -94,10 +97,11 @@ def pdp_setup(*args):
     # wait for the cloud's respond
     wait_ack(server_sock)
 
-# send 'blocksize' 'block_count' and file named 'file_name' to the cloud(server_sock)
-# blocksize is the size(bytes) of the block of the file
-# block_count is global
 def sendto_cloud(file_name, blocksize, server_sock):
+    """send 'blocksize' 'block_count' and file named 'file_name' to the cloud(server_sock)
+    argument is same as above
+    block_count -- global variable
+    """
     global block_count
     with open(file_name, 'rb') as fp:
         # send blocksize first and the number of blocks second
@@ -109,9 +113,9 @@ def sendto_cloud(file_name, blocksize, server_sock):
             if not data: break
             server_sock.send(data)
 
-# send 'Done' tell cloud no data sent any more
-# and the cloud should reply 'All Received'
 def wait_ack(server_sock):
+    """send 'Done' tell cloud no data sent any more
+    and the cloud should reply 'All Received'"""
     try:
         server_sock.send('Done')
         reply = server_sock.recv(BUFF_SIZE)
@@ -123,9 +127,9 @@ def wait_ack(server_sock):
     else:
         print 'Get Wrong React From Cloud'
 
-# divide file named 'file_name' into blocks, then calculate tags
-# save tags in a new file named 'output_name'
 def gen_metadata(file_name, output_name, blocksize = BLOCKSIZE, mode = 0):
+    """divide file named 'file_name' into blocks, then calculate tags and
+    save tags in a new file named 'output_name'"""
     key = gen_key(KEY_LEN)
     tag_key = get_tag_key(key)
     if not tag_key:
@@ -135,9 +139,9 @@ def gen_metadata(file_name, output_name, blocksize = BLOCKSIZE, mode = 0):
     store_file_tag(tag_list, output_name, blocksize)
     store_key(key, 'key')
 
-# generate the tag for each block of file named 'file_name'
-# 'key' is the key used for tag generating
 def gen_file_tag(file_name, blocksize, key, hash_func = SHA256):
+    """generate the tag(list) for each block of file named 'file_name'
+    'key' is the key used for tag generating"""
     global block_count 
     tag_list = []
     with open(file_name, 'rb') as fp:
@@ -149,21 +153,22 @@ def gen_file_tag(file_name, blocksize, key, hash_func = SHA256):
     block_count = len(tag_list)
     return tag_list
 
-# store block size, tag length and all tags in file named 'output_name'
-# output file structure: blocksize-TAG_LEN-tag1-tag2-...-tagn
-# 'tag_list' is all tags
 def store_file_tag(tag_list, output_name, blocksize):
+    """store block size, tag length and all tags in file named 'output_name'
+    output file structure: blocksize-TAG_LEN-tag1-tag2-...-tagn
+    tag_list -- all tags in list
+    """
     with open(output_name, 'wb') as fp:
         fp.write(struct.pack('i', blocksize))
         fp.write(struct.pack('i', TAG_LEN))
         for tag in tag_list:
             fp.write(tag)
 
-# generate a tag for 'block' with 'key'
-# 'key' should be cryptografic key
-# 'hash_func' is hash algorithm
-# return the tag
 def gen_tag(block, key, hash_func = SHA256):
+    """generate a tag for 'block' with 'key', return the tag in bytes
+    key -- cryptografic key
+    hash_func -- hash algorithm
+    """
     key = random.long_to_bytes(key)
     Mac = HMAC.new(key, block, hash_func)
     tag = Mac.digest()
